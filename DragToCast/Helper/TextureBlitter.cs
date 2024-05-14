6fc42aa1ac18d33789d078c44ffbb68c8016e890
DK <35783662+gottyduke@users.noreply.github.com>
2024-05-14 11:53:01 +0800

    feat: small refactor of `TextureBlitter` helper

--- DragToCast/Helper/TextureBlitter.cs
+++ DragToCast/Helper/TextureBlitter.cs
@@ -10,34 +10,42 @@ internal static class TextureBlitter
     private static readonly Dictionary<string, Texture2D> _cached = [];
 
     /// <summary>
-    /// Blit a new Texture2D from a packed texture atlas with given rect
+    /// Blit a new Texture2D from a packed texture atlas with given rect.
     /// </summary>
     /// <param name="sourceTexture">packed texture atlas</param>
     /// <param name="textureRect">dst rect</param>
-    /// <returns>Unpacked Texture2D from atlas. Result is cached</returns>
+    /// <returns>Unpacked Texture2D from atlas. Result is cached.</returns>
     internal static Texture2D Blit(this Texture2D sourceTexture, Rect textureRect)
     {
         var id = sourceTexture.name + textureRect.ToString();
-        if (_cached.ContainsKey(id)) {
-            return _cached[id];
+        if (_cached.TryGetValue(id, out var cachedTexture)) {
+            return cachedTexture;
         }
 
+        var textureToReadFrom = sourceTexture;
+        bool createdTemporaryTexture = false;
+
         if (!sourceTexture.isReadable) {
-            var rrTexture = RenderTexture.GetTemporary(
+            var renderTexture = RenderTexture.GetTemporary(
                 sourceTexture.width,
                 sourceTexture.height,
                 0,
                 RenderTextureFormat.Default,
                 RenderTextureReadWrite.Linear
             );
-            Graphics.Blit(sourceTexture, rrTexture);
+            Graphics.Blit(sourceTexture, renderTexture);
+
             var backup = RenderTexture.active;
-            RenderTexture.active = rrTexture;
-            sourceTexture = new Texture2D(sourceTexture.width, sourceTexture.height);
-            sourceTexture.ReadPixels(new(0, 0, rrTexture.width, rrTexture.height), 0, 0);
-            sourceTexture.Apply();
+            RenderTexture.active = renderTexture;
+
+            textureToReadFrom = new Texture2D(sourceTexture.width, sourceTexture.height);
+            textureToReadFrom.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+            textureToReadFrom.Apply();
+
             RenderTexture.active = backup;
-            RenderTexture.ReleaseTemporary(rrTexture);
+            RenderTexture.ReleaseTemporary(renderTexture);
+
+            createdTemporaryTexture = true;
         }
 
         var x = Mathf.FloorToInt(textureRect.x);
@@ -46,11 +54,14 @@ internal static class TextureBlitter
         var height = Mathf.FloorToInt(textureRect.height);
         var subTexture = new Texture2D(width, height, TextureFormat.ARGB32, false);
 
-        subTexture.SetPixels(sourceTexture.GetPixels(x, y, width, height));
+        subTexture.SetPixels(textureToReadFrom.GetPixels(x, y, width, height));
         subTexture.Apply();
-        _cached[id] = subTexture;
 
+        if (createdTemporaryTexture) {
+            Object.DestroyImmediate(textureToReadFrom);
+        }
+
+        _cached[id] = subTexture;
         return subTexture;
     }
-
 }
